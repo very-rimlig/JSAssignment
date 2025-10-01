9926a0cc55f1c23940e4e32f07a113752cd82fb0
very-rimlig <josephine.bergenmar@gmail.com>
2025-10-01 16:23:15 +0200

    Update app.js

--- P5Operators/js/app.js
+++ P5Operators/js/app.js
@@ -26,3 +26,23 @@ let langd = 1.82;
 let bmi = vikt / (langd * langd);
 
 console.log("Ditt BMI är:", bmi.toFixed(2));
+
+//Implement a program that calculates the length of the hypotenuse of a right triangle using the lengths of the other two sides as inputs. Display the result.
+let x = 3;
+let y = 4;
+
+// Hypotenusan = roten ur (x^2 + y^2)
+let hypotenuse = Math.sqrt(x * x + y * y);
+
+console.log("Hypotenusan är:", hypotenuse);
+
+//Implement a program that takes a number input representing seconds and converts it to minutes and remaining seconds, then displays the result
+
+
+let totsekunder = 111;
+
+let minuter = Math.floor(totsekunder / 60); // Hela minuter
+
+let sekunderkvar = totsekunder % 60;             // Resten som blir över, sekunder
+
+console.log(minuter + " minuter och " + sekunderkvar + " sekunder");
