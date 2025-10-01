85ae3e2f9539d84fe39f92fd019107cacc7357fc
very-rimlig <josephine.bergenmar@gmail.com>
2025-10-01 11:11:12 +0200

    övning

--- js/app.js
+++ js/app.js
@@ -1,10 +1,46 @@
 //Namn: Josephine Bergenmar//
 //Datum: 2025-10-01//
 //Mejl: josephine.bergenmar@gmail.com//
-
+/*
 console.log(
   "hello world"
 );
 let namn = "Josephine";
 console.log("Det är " + namn + " skriver detta");
 console.log("Jag vill ta ett snack för magen kurrar och klockan är kvart över tio.");
+
+let stringExample; //variabeltypen kan sättas först eller sist, ordning spelar ingen roll?
+
+stringExample = "Hello class";
+*/
+
+
+/*
+let numberOfBottles;
+numberOfBottles = 10;
+
+const line1;
+line1 = "green bottles hanging on the wall";
+
+const line2;
+line2 = "and if one green bottle, should accidentally fall, there will be";
+
+console.log(numberOfBottles);
+console.log(line1);
+console.log(numberOfBottles);
+*/
+
+let numberOfBottles;
+numberOfBottles = 10;
+
+const line1 = "green bottles hanging on the wall";
+const line2 = "and if one green bottle, should accidentally fall, there will be";
+
+console.log(numberOfBottles);
+console.log(line1);
+console.log(numberOfBottles);
+console.log(line2);
+console.log(numberOfBottles -1);
+console.log(line1);
+
+
