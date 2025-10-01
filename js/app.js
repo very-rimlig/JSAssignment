bb07667a749d01c2dce4ca55dbc9097bd040b501
very-rimlig <josephine.bergenmar@gmail.com>
2025-10-01 10:18:39 +0200

    Update app.js
    testade console.log

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 console.log(
   "hello world"
 );
+let namn = "Josephine";
+console.log("Det är " + namn + " skriver detta");
+console.log("Jag vill ta ett snack för magen kurrar och klockan är kvart över tio.");
