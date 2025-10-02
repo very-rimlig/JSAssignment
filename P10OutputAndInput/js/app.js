4aedb4750e53077f255e22d45d0b08e12976c1f4
very-rimlig <josephine.bergenmar@gmail.com>
2025-10-02 15:38:40 +0200

    fler övningar
    fortsätter övningarna

---
+++ P10OutputAndInput/js/app.js
@@ -0,0 +1,11 @@
+function dubbleratal(numberToDouble) {
+  return numberToDouble * 2;
+}
+console.log(dubbleratal(4)); //Output:8
+
+//Här gör jag en funktion där siffran som stoppas in i parametern multipliceras med sig själv och returners och skrivs ut
+function multiplyWithIS(numberToMult) {
+  return numberToMult * numberToMult;
+}
+console.log(multiplyWithIS(4));
+
