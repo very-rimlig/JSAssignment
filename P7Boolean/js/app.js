23dccbae494db3b283926bd42e425a1395bc90d9
very-rimlig <josephine.bergenmar@gmail.com>
2025-10-02 12:10:28 +0200

    update
    första stegen i övning

---
+++ P7Boolean/js/app.js
@@ -0,0 +1,10 @@
+//Goal -> you're child = fales, you´re teen = false, you're an adult = true
+
+
+
+let birthYear = 2000; //födelseår ska kunna ändras såklar
+const currentYear = 2025; //const eftersom vi ska utgå från att inte ändra denna
+let age = currentYear - birthYear; //kommentar överflödig
+console.log(age); //testar om uträkning av ålder stämmer
+
+
--- p6types/js/app.js
+++ p6types/js/app.js
@@ -1,4 +1,4 @@
-/* Det här är min kod för att skriva ut ramsan, utan for-loop
+/* Det här var min kod som jag först gjorde den
 let numberOfBottles;
 numberOfBottles = 10;
 
@@ -11,13 +11,23 @@ console.log(line2);
 console.log(numberOfBottles -1 + line1);
 */
 
-//Det här är min kod för att skriva ut ramsan med färre kod-rader
+/*Det här är min kod för att skriva ut ramsan med färre kod-rader
 let numberOfBottles = 10;
 
 console.log(numberOfBottles + " green bottles hanging on the wall. "
   + numberOfBottles + " green bottles hanging on the wall.");
 console.log("And if one green bottle should accidentally fall, there will be "
   + (numberOfBottles - 1) + " green bottles hanging on the wall.");
+*/
 
-//Det här är min kod för att skriva ut ramsan med hjälp av en loop
+//Det här är min kod för att skriva ut ramsan med hjälp av en for-loop
+let numberOfBottles = 10;
 
+while (numberOfBottles > 0) {
+  console.log(`${numberOfBottles} green bottles hanging on the wall. ${numberOfBottles} green bottles hanging on the wall.`);
+  console.log(`And if one green bottle should accidentally fall, there will be ${numberOfBottles - 1} green bottles hanging on the wall.`);
+  console.log(" La, La, La Laaa "); //  rad mellan verserna med lalala
+
+  numberOfBottles--; // minska antalet
+}
+console.log("Now, there are no more bottles left on the wall, tralalala");
