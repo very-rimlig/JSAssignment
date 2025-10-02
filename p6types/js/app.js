432832628c2f203688c951fa54e01597b1ceeb7b
very-rimlig <josephine.bergenmar@gmail.com>
2025-10-02 10:33:43 +0200

    uppgift
    lägger till lösning av uppgift

---
+++ p6types/js/app.js
@@ -0,0 +1,23 @@
+/* Det här är min kod för att skriva ut ramsan, utan for-loop
+let numberOfBottles;
+numberOfBottles = 10;
+
+const line1 = " green bottles hanging on the wall.";
+const line2 = "And if one green bottle, should accidentally fall, there will be";
+
+console.log(numberOfBottles + line1);
+console.log(numberOfBottles + line1);
+console.log(line2);
+console.log(numberOfBottles -1 + line1);
+*/
+
+//Det här är min kod för att skriva ut ramsan med färre kod-rader
+let numberOfBottles = 10;
+
+console.log(numberOfBottles + " green bottles hanging on the wall. "
+  + numberOfBottles + " green bottles hanging on the wall.");
+console.log("And if one green bottle should accidentally fall, there will be "
+  + (numberOfBottles - 1) + " green bottles hanging on the wall.");
+
+//Det här är min kod för att skriva ut ramsan med hjälp av en loop
+
