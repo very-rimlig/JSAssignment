91521934814ed5b1a9a9551c7dc85c92a8de5752
very-rimlig <josephine.bergenmar@gmail.com>
2025-10-02 12:39:10 +0200

    slutförde övning
    sista delen av övning i boolean

--- P7Boolean/js/app.js
+++ P7Boolean/js/app.js
@@ -2,7 +2,7 @@
 
 
 
-let birthYear = 2000; //födelseår ska kunna ändras såklar
+let birthYear = 2024; //födelseår ska kunna ändras såklar
 const currentYear = 2025; //const eftersom vi ska utgå från att inte ändra denna
 let age = currentYear - birthYear; //kommentar överflödig
 console.log(age); //testar om uträkning av ålder stämmer
@@ -15,7 +15,25 @@ let isChild = age <= 10;
 let isTeen = age >=11 && age <= 17;
 let isAdult = age >= 18;
 
-
+/*
 console.log(isChild);
 console.log(isTeen);
 console.log(isAdult);
+*/
+
+console.log("Ålder:", age);
+console.log("Barn:", isChild);
+console.log("Tonåring:", isTeen);
+console.log("Vuxen:", isAdult);
+
+// Kontrollera om födelseår är efter 2025 eller innan 1925 "extremvärden"
+if (birthYear > currentYear || birthYear < 1925) {
+  console.log("Ogiltigt födelseår! Kontrollera att det är mellan 1925 och 2025.");
+} else {
+  // Kolla om personen är vuxen
+  if (isAdult) {
+    console.log("Du är välkommen att köpa öl!");
+  } else {
+    console.log("Du får inte köpa alkohol!");
+  }
+}
