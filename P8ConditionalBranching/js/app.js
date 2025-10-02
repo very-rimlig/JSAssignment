355ef54c0f81c92f8204d691be3b08d7955f7f3f
very-rimlig <josephine.bergenmar@gmail.com>
2025-10-02 13:26:54 +0200

    Update app.js

--- P8ConditionalBranching/js/app.js
+++ P8ConditionalBranching/js/app.js
@@ -1,8 +1,8 @@
 // outputs: Your are an adult, The greater number is: 24, Your favorite number is an odd number
 
 
-let birthYear = 1987;
-let favoriteNumber = 38;
+let birthYear = 2011;
+let favoriteNumber = 39;
 
 const currentYear = 2025; //const eftersom vi ska utgå från att inte ändra denna
 let age = currentYear - birthYear; //kommentar överflödig
@@ -10,6 +10,23 @@ let age = currentYear - birthYear; //kommentar överflödig
 let isChild = age <= 10;
 let isTeen = age >=11 && age <= 17;
 let isAdult = age >= 18;
+
+if (birthYear > currentYear || birthYear < 1925) {
+  console.log("Ogiltigt födelseår! Kontrollera att det är mellan 1925 och 2025.");
+}
+
+/*
+//nu prövar vi detta med udda och jämna tal
+if (favoriteNumber % 2 === 0) {
+  console.log("Ditt favoritnummer är jämnt.");
+} else {
+  console.log("Ditt favoritnummer är udda.");
+}
+*/
+
+// nu prövar vi med conditional (ternary op)
+console.log(favoriteNumber % 2 === 0 ? "Ditt favoritnummer är jämnt." : "Ditt favoritnummer är udda.");
+
 
 
 if (isAdult) {
