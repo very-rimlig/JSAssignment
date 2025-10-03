b64528ba64de926b305b3d75f3b6198cf4de2feb
very-rimlig <josephine.bergenmar@gmail.com>
2025-10-03 13:59:08 +0200

    FINNISH
    slutför uppgiften

--- Assignment2-minigame/js/app.js
+++ Assignment2-minigame/js/app.js
@@ -6,18 +6,46 @@ let male = false;
 function main(name, age, female, male) {
   greeting(name);
   checkage(age);
+  giveRandomBook();
 }
 
+// Hälsning
+function greeting(name) {
+  console.log("Hej " + name);
+}
+
+// Ålderskontroll + specialmeddelande
 function checkage(age) {
   if (age < 20) {
     console.log("Du är tyvärr för ung för att spela Boklotteriet");
   } else {
     console.log("Välkommen in till boklotteriet");
+
+    if (age > 30) {
+      console.log("Du uppgraderas till superläsare!");
+    }
   }
 }
 
-function greeting(name) {
-  console.log("Hej " + name);
+// Slumpa en bok från listan
+function giveRandomBook() {
+  const books = [
+    "Sagan om Sune",
+    "Bajsboken",
+    "Kvinnorummet",
+    "Lagboken",
+    "Röda rummet",
+    "Familjen",
+    "Lär dig koda eller göm dig",
+    "Regler är till för att brytas",
+    "På järnväg genom ett regnigt Närke",
+    "Kokboken"
+  ];
+
+  let randomIndex = Math.floor(Math.random() * books.length);
+  let chosenBook = books[randomIndex];
+
+  console.log("Du får läsa: " + chosenBook);
 }
 
 main(profileName, profileAge, female, male);
